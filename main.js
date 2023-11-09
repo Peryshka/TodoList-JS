deaa7982cf1e6830dfcca95274b8cc487ecd8212
Perishte <javascript_9595@bk.ru>
2023-11-09 16:24:38 +0500

    add,edit,delete,clear-all todo elems

--- main.js
+++ main.js
@@ -1,49 +1,94 @@
 const form = document.forms['form'];
-const todoAddEl = form['input-text'];
-const addBtn = form['addBtn'];
-const inputWrap = document.getElementsByClassName('custom-input')[0];
-const todoList = document.getElementsByClassName('list')[0];
-window.addEventListener('beforeunload', (event) => {
-  event.preventDefault();
-  event.returnValue = '';
-});
-addBtn.addEventListener('click', function(e){
-        e.preventDefault();
-  const inputContent = todoAddEl.value
-  const listItem = document.createElement('li');
-  listItem.textContent = inputContent;
-  if(listItem.textContent==="") {
-    const error = document.createElement('span')
-    error.classList.add('error');
-    error.textContent = 'Please enter todo task!';
-    inputWrap.prepend(error.textContent);
-    error.value = "";
-    return;
+const input = form['input'];
+const addlist =  form['addBtn'];
+const formWrap = document.getElementsByClassName('custom-input')[0];
+const todoList = document.querySelector('.list');
+const clearAll = document.getElementsByClassName('clear-all')[0];
+let editElem;
+let editFlag;
+//Event Listeners
+form.addEventListener('submit', addListItem);
+clearAll.addEventListener('click', clearAllList);
+
+//Functions
+//Add function
+function addListItem(e) {
+  e.preventDefault();
+  const listValue = input.value.trim();
+  const createdTime = currentTime();
+  if(listValue && !editFlag){
+    createItem(listValue, createdTime)
+     defaultSettings()
+  } else if(listValue && editFlag) {
+    editElem.textContent = listValue;
+    defaultSettings();
+  }else if(!listValue){
+    let error = document.createElement('span');
+    error.textContent='Please enter task for TODO List!';
+    formWrap.prepend(error.textContent);
   }
+}
+
+//function for getting current time
+function currentTime() {
   const date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  hours = (hours < 10) ? "0" + hours : hours;
-  minutes = (minutes < 10) ? "0" + minutes : minutes;
-  const time = ` - ${hours} : ${minutes}`;
-  listItem.classList.add('listItem');
-  const deleteBtn = document.createElement('span');
-  deleteBtn.classList.add('addDeleteIcon');
-  listItem.append(time);
-  listItem.append(deleteBtn);
-  todoList.append(listItem);
-  //todoAddEl.value = "";
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  return `${hours}:${minutes}`;
+}
 
-  todoList.addEventListener('click', function(e) {
-    if(e.target.classList.contains('addDeleteIcon')){
-      const element = e.target.parentElement;
-      todoList.removeChild(element);
-    }
-  });
-});
+//function to create Item
+function createItem(listValue, createdTime) {
+  const listItem = document.createElement('li');
+  listItem.classList.add('list-item');
+  listItem.innerHTML = `
+    <span>
+     <span>${listValue}</span>
+     - ${createdTime}</span>
+          <div class="btn-container">
+            <button class="edit-btn">
+              Edit
+            </button>
+            <i class="delete-icon">
+            </i>
+          </div>
+  `;
+  todoList.append(listItem)
+  const deleteItem = listItem.querySelector('.delete-icon');
+  deleteItem.addEventListener('click' , removeElement);
+  const editItem = listItem.querySelector('.edit-btn');
+  editItem.addEventListener('click', editElement);
+}
 
+//function to delete Item
+function removeElement(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  element.remove();
+}
 
+//function to clear all items
+function clearAllList(e) {
+  const elements = document.querySelectorAll('.list-item');
+  elements.forEach(item => {
+    item.remove();
+  })
+};
 
+//function to edit element
+function editElement(e) {
+  let element = e.currentTarget.parentElement.previousElementSibling.querySelector('span');
+  editElem = element;
+  input.value = editElem.textContent;
+  addlist.textContent = 'Edit';
+  editFlag = true;
+}
+
+//function for default settings
+function defaultSettings() {
+  input.value = "";
+  editFlag = false;
+  addList.textContent = 'Add new task';
+}
 
 
 
